f93b09c55d7304395090fc9e5b3eb5df7e83ead7
Lee Marlow <lee.marlow@gmail.com>
2021-12-05 14:43:22 -0700

    Solve 2021-04 in livebook

---
+++ lib/2021/day_04.ex
@@ -0,0 +1,62 @@
+defmodule AdventOfCode.Y2021.Day04 do
+  @moduledoc """
+  --- Day 4: Giant Squid ---
+  Problem Link: https://adventofcode.com/2021/day/4
+  """
+  use AdventOfCode.Helpers.InputReader, year: 2021, day: 4
+
+  @doc ~S"""
+  Sample data:
+
+  ```
+  7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+  22 13 17 11  0
+  8  2 23  4 24
+  21  9 14 16  7
+  6 10  3 18  5
+  1 12 20 15 19
+
+  3 15  0  2 22
+  9 18 13 17  5
+  19  8  7 25 23
+  20 11 10 24  4
+  14 21 16 12  6
+
+  14 21 17 24  4
+  10 16 15  9 19
+  18  8 23 26 20
+  22 11 13  6  5
+  2  0 12  3  7
+  ```
+  """
+  def run(data \\ input!(), part)
+
+  def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
+
+  def run(data, part) when is_list(data), do: data |> solve(part)
+
+  def parse(data) do
+    data
+    |> String.split("\n", trim: true)
+  end
+
+  def solve(data, 1), do: solve_1(data)
+  def solve(data, 2), do: solve_2(data)
+
+  # --- <Solution Functions> ---
+
+  @doc """
+  """
+  def solve_1(data) do
+    {1, :not_implemented}
+  end
+
+  @doc """
+  """
+  def solve_2(data) do
+    {2, :not_implemented}
+  end
+
+  # --- </Solution Functions> ---
+end
