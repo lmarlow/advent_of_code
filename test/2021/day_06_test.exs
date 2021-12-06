a68b1cdff929d554524296af0e109136dad19e5b
Lee Marlow <lee.marlow@gmail.com>
2021-12-06 01:29:08 -0700

    Solve 2021-06 part 2

--- test/2021/day_06_test.exs
+++ test/2021/day_06_test.exs
@@ -11,21 +11,21 @@ defmodule AdventOfCode.Y2021.Day06Test do
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 5934
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 352_195
     end
   end
 
   describe "part 2" do
     test "example" do
-      assert Solution.run(@sample_data, 2) == nil
+      assert Solution.run(@sample_data, 2) == 26_984_457_539
     end
 
     test "input file" do
-      assert Solution.run(2) == nil
+      assert Solution.run(2) == 1_600_306_001_288
     end
   end
 end
