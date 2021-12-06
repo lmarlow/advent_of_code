9c8312364be25b0dbfa5464971ceef5ed1b8839b
Lee Marlow <lee.marlow@gmail.com>
2021-12-05 17:04:45 -0700

    2021-05 from livemd to solution module

--- lib/2021/day_05.ex
+++ lib/2021/day_05.ex
@@ -20,6 +20,10 @@ defmodule AdventOfCode.Y2021.Day05 do
   def parse(data) do
     data
     |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&Regex.scan(~r/\d+/, &1))
+    |> Enum.map(&Enum.map(&1, fn [n] -> String.to_integer(n) end))
+    |> Enum.map(&Enum.chunk_every(&1, 2))
   end
 
   def solve(data, 1), do: solve_1(data)
@@ -30,14 +34,32 @@ defmodule AdventOfCode.Y2021.Day05 do
   @doc """
   """
   def solve_1(data) do
-    {1, :not_implemented}
+    data
+    |> Enum.filter(fn [[x1, y1], [x2, y2]] -> x1 == x2 or y1 == y2 end)
+    |> count_dangerous_points()
   end
 
   @doc """
   """
   def solve_2(data) do
-    {2, :not_implemented}
+    data
+    |> count_dangerous_points()
   end
 
   # --- </Solution Functions> ---
+
+  def points([x1, y1], [x2, y1]), do: Enum.zip(x1..x2, List.duplicate(y1, Enum.count(x1..x2)))
+
+  def points([x1, y1], [x1, y2]), do: Enum.zip(List.duplicate(x1, Enum.count(y1..y2)), y1..y2)
+
+  def points([x1, y1], [x2, y2]), do: Enum.zip(x1..x2, y1..y2)
+
+  defp count_dangerous_points(points) do
+    points
+    |> Enum.flat_map(fn [p1, p2] -> points(p1, p2) end)
+    |> Enum.reduce(%{}, fn p, acc ->
+      Map.update(acc, p, 1, &(&1 + 1))
+    end)
+    |> Enum.count(fn {_, n} -> n > 1 end)
+  end
 end
--- test/2021/day_05_test.exs
+++ test/2021/day_05_test.exs
@@ -7,25 +7,35 @@ defmodule AdventOfCode.Y2021.Day05Test do
   alias AdventOfCode.Y2021.Day05, as: Solution
 
   @sample_data ~S"""
+  0,9 -> 5,9
+  8,0 -> 0,8
+  9,4 -> 3,4
+  2,2 -> 2,1
+  7,0 -> 7,4
+  6,4 -> 2,0
+  0,9 -> 2,9
+  3,4 -> 1,4
+  0,0 -> 8,8
+  5,5 -> 8,2
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 5
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 7142
     end
   end
 
   describe "part 2" do
     test "example" do
-      assert Solution.run(@sample_data, 2) == nil
+      assert Solution.run(@sample_data, 2) == 12
     end
 
     test "input file" do
-      assert Solution.run(2) == nil
+      assert Solution.run(2) == 20012
     end
   end
 end
