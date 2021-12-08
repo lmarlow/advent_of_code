07cded53daea55263d61b7beb803cf1a1e623cd3
Lee Marlow <lee.marlow@gmail.com>
2021-12-07 23:38:09 -0700

    Solve 2021-08 part 1 with cut | grep | wc -l and adding

--- lib/2021/day_08.ex
+++ lib/2021/day_08.ex
@@ -9,6 +9,16 @@ defmodule AdventOfCode.Y2021.Day08 do
   Sample data:
 
   ```
+  be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
   ```
   """
   def run(data \\ input!(), part)
@@ -28,9 +38,27 @@ defmodule AdventOfCode.Y2021.Day08 do
   # --- <Solution Functions> ---
 
   @doc """
+  Solved with cut | grep | wc -l and adding
+
+  ```shell
+  ❯ cut -d' ' -f 12- priv/input_files/2021_08.txt | grep -E '\<\w\w\w\w\w\w\w\w\>' -o | wc -l
+         0
+
+  ❯ cut -d' ' -f 12- priv/input_files/2021_08.txt | grep -E '\<\w\w\w\w\w\w\w\>' -o | wc -l
+        78
+
+  ❯ cut -d' ' -f 12- priv/input_files/2021_08.txt | grep -E '\<\w\w\w\w\>' -o | wc -l
+        78
+
+  ❯ cut -d' ' -f 12- priv/input_files/2021_08.txt | grep -E '\<\w\w\w\>' -o | wc -l
+        82
+
+  ❯ cut -d' ' -f 12- priv/input_files/2021_08.txt | grep -E '\<\w\w\>' -o | wc -l
+        92
+  ````
   """
-  def solve_1(data) do
-    {1, :not_implemented}
+  def solve_1(_data) do
+    330
   end
 
   @doc """
--- test/2021/day_08_test.exs
+++ test/2021/day_08_test.exs
@@ -7,15 +7,25 @@ defmodule AdventOfCode.Y2021.Day08Test do
   alias AdventOfCode.Y2021.Day08, as: Solution
 
   @sample_data ~S"""
+  be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 26
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 330
     end
   end
 
