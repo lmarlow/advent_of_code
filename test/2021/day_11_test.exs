945a1b597cefab0c74899c759ad7b252a15e3f4d
Lee Marlow <lee.marlow@gmail.com>
2021-12-23 00:09:06 -0700

    Solve 2021-11 part 1

--- test/2021/day_11_test.exs
+++ test/2021/day_11_test.exs
@@ -7,11 +7,21 @@ defmodule AdventOfCode.Y2021.Day11Test do
   alias AdventOfCode.Y2021.Day11, as: Solution
 
   @sample_data ~S"""
+  5483143223
+  2745854711
+  5264556173
+  6141336146
+  6357385478
+  4167524645
+  2176841721
+  6882881134
+  4846848554
+  5283751526
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 1656
     end
 
     test "input file" do
