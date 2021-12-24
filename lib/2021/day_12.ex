7763119d66ddadb849087e535518ade088aa5d5d
Lee Marlow <lee.marlow@gmail.com>
2021-12-24 10:43:54 -0700

    Solve 2021-12 part 1

--- lib/2021/day_12.ex
+++ lib/2021/day_12.ex
@@ -15,11 +15,12 @@ defmodule AdventOfCode.Y2021.Day12 do
 
   def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
 
-  def run(data, part) when is_list(data), do: data |> solve(part)
+  def run(data, part), do: data |> solve(part)
 
   def parse(data) do
     data
     |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, "-"))
   end
 
   def solve(data, 1), do: solve_1(data)
@@ -30,7 +31,10 @@ defmodule AdventOfCode.Y2021.Day12 do
   @doc """
   """
   def solve_1(data) do
-    {1, :not_implemented}
+    data
+    |> nodes()
+    |> paths()
+    |> length()
   end
 
   @doc """
@@ -40,4 +44,41 @@ defmodule AdventOfCode.Y2021.Day12 do
   end
 
   # --- </Solution Functions> ---
+
+  def nodes(lines) do
+    lines
+    |> Enum.reduce(%{}, fn
+      [a, b], acc ->
+        acc
+        |> Map.update(a, [b], &[b | &1])
+        |> Map.update(b, [a], &[a | &1])
+    end)
+    |> Enum.map(fn {k, v} -> {k, v -- ["start"]} end)
+    |> Map.new()
+    |> Map.delete("end")
+  end
+
+  def paths(nodes) do
+    "start"
+    |> paths(nodes, [])
+    |> List.flatten()
+  end
+
+  def paths("end", _nodes, path), do: ["end" | path] |> Enum.reverse() |> List.to_tuple()
+
+  def paths(current_node, nodes, path) do
+    if allowed?(current_node, path) do
+      path = [current_node | path]
+
+      for next <- nodes[current_node] do
+        paths(next, nodes, path)
+      end
+    else
+      []
+    end
+  end
+
+  defp allowed?(node, path) do
+    String.match?(node, ~r/[A-Z]+/) or node not in path
+  end
 end
--- test/2021/day_12_test.exs
+++ test/2021/day_12_test.exs
@@ -7,15 +7,64 @@ defmodule AdventOfCode.Y2021.Day12Test do
   alias AdventOfCode.Y2021.Day12, as: Solution
 
   @sample_data ~S"""
+  fs-end
+  he-DX
+  fs-he
+  start-DX
+  pj-DX
+  end-zg
+  zg-sl
+  zg-pj
+  pj-he
+  RW-he
+  fs-DX
+  pj-RW
+  zg-RW
+  start-pj
+  he-WI
+  zg-he
+  pj-fs
+  start-RW
   """
 
   describe "part 1" do
+    test "simple 10" do
+      data = """
+      A-start
+      start-b
+      A-c
+      A-b
+      b-d
+      A-end
+      b-end
+      """
+
+      assert Solution.run(data, 1) == 10
+    end
+
+    test "simple 19" do
+      data = """
+      dc-end
+      HN-start
+      start-kj
+      dc-start
+      dc-HN
+      LN-dc
+      HN-end
+      kj-sa
+      kj-HN
+      kj-dc
+      """
+
+      assert Solution.run(data, 1) == 19
+    end
+
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 226
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 4495
     end
   end
 
