5eaa0ef0336fc25fd75fd6f731a82d750eba3f7c
Lee Marlow <lee.marlow@gmail.com>
2021-12-26 16:20:59 -0700

    Solve 2021-13 part 1

--- test/2021/day_13_test.exs
+++ test/2021/day_13_test.exs
@@ -7,11 +7,32 @@ defmodule AdventOfCode.Y2021.Day13Test do
   alias AdventOfCode.Y2021.Day13, as: Solution
 
   @sample_data ~S"""
+  6,10
+  0,14
+  9,10
+  0,3
+  10,4
+  4,11
+  6,0
+  6,12
+  4,1
+  0,13
+  10,12
+  3,4
+  3,0
+  8,4
+  1,10
+  2,14
+  8,10
+  9,0
+
+  fold along y=7
+  fold along x=5
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 17
     end
 
     test "input file" do
