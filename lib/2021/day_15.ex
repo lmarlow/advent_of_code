e1fe48cb895e2cf04e212b5f16cda0e1a543ddb8
Lee Marlow <lee.marlow@gmail.com>
2021-12-31 16:58:28 -0700

    Solve 2021-15 part 1

--- lib/2021/day_15.ex
+++ lib/2021/day_15.ex
@@ -9,17 +9,34 @@ defmodule AdventOfCode.Y2021.Day15 do
   Sample data:
 
   ```
+  1163751742
+  1381373672
+  2136511328
+  3694931569
+  7463417111
+  1319128137
+  1359912421
+  3125421639
+  1293138521
+  2311944581
   ```
   """
   def run(data \\ input!(), part)
 
   def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
 
-  def run(data, part) when is_list(data), do: data |> solve(part)
+  def run(data, part), do: data |> solve(part)
 
   def parse(data) do
-    data
-    |> String.split("\n", trim: true)
+    lines =
+      data
+      |> String.split("\n", trim: true)
+
+    for {line, row_num} <- Enum.with_index(lines),
+        {char_val, col_num} <- Enum.with_index(String.to_charlist(line)),
+        into: %{} do
+      {{row_num, col_num}, char_val - ?0}
+    end
   end
 
   def solve(data, 1), do: solve_1(data)
@@ -29,14 +46,52 @@ defmodule AdventOfCode.Y2021.Day15 do
 
   @doc """
   """
-  def solve_1(data) do
-    {1, :not_implemented}
+  def solve_1(grid) do
+    rows =
+      grid
+      |> Enum.map(fn {{row, _}, _} -> row end)
+      |> Enum.max()
+
+    cols =
+      grid
+      |> Enum.map(fn {{_, col}, _} -> col end)
+      |> Enum.max()
+
+    path({0, 0}, {rows, cols}, grid)
   end
 
   @doc """
   """
-  def solve_2(data) do
+  def solve_2(_data) do
     {2, :not_implemented}
+  end
+
+  def path(start, target, grid) do
+    distances = %{start => 0}
+    queue = MapSet.new(Map.keys(grid))
+    path(grid, target, distances, queue)
+  end
+
+  def path(grid, target, distances, queue) do
+    {row, col} = u = Enum.min_by(queue, &distances[&1])
+
+    if u == target do
+      distances[u]
+    else
+      queue = MapSet.delete(queue, u)
+
+      distances =
+        for v <- [{row - 1, col}, {row + 1, col}, {row, col - 1}, {row, col + 1}],
+            v in queue,
+            alt = grid[v] + distances[u],
+            alt < Map.get(distances, v, :infinity),
+            reduce: distances do
+          distances ->
+            Map.put(distances, v, alt)
+        end
+
+      path(grid, target, distances, queue)
+    end
   end
 
   # --- </Solution Functions> ---
--- test/2021/day_15_test.exs
+++ test/2021/day_15_test.exs
@@ -7,15 +7,35 @@ defmodule AdventOfCode.Y2021.Day15Test do
   alias AdventOfCode.Y2021.Day15, as: Solution
 
   @sample_data ~S"""
+  1163751742
+  1381373672
+  2136511328
+  3694931569
+  7463417111
+  1319128137
+  1359912421
+  3125421639
+  1293138521
+  2311944581
   """
 
   describe "part 1" do
+    test "tiny" do
+      data = """
+      119
+      139
+      211
+      """
+
+      assert Solution.run(data, 1) == 5
+    end
+
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 40
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 609
     end
   end
 
