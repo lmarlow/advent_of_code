3b48357db0d94a2d2936dc396cc11811f2393ace
Lee Marlow <lee.marlow@gmail.com>
2022-01-09 01:38:38 -0700

    Close to 2021-16 part 1

--- lib/2021/day_16.ex
+++ lib/2021/day_16.ex
@@ -5,6 +5,8 @@ defmodule AdventOfCode.Y2021.Day16 do
   """
   use AdventOfCode.Helpers.InputReader, year: 2021, day: 16
 
+  defstruct version: 0, type_id: 0, length_type_id: 0, length: 0, value: nil, leftover: <<>>
+
   @doc ~S"""
   Sample data:
 
@@ -13,14 +15,15 @@ defmodule AdventOfCode.Y2021.Day16 do
   """
   def run(data \\ input!(), part)
 
-  def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
+  def run(data, part), do: data |> parse() |> solve(part)
 
-  def run(data, part) when is_list(data), do: data |> solve(part)
-
-  def parse(data) do
+  def parse(data) when is_binary(data) do
     data
-    |> String.split("\n", trim: true)
+    |> Base.decode16!()
   end
+
+  def parse(data) when is_bitstring(data),
+    do: data
 
   def solve(data, 1), do: solve_1(data)
   def solve(data, 2), do: solve_2(data)
@@ -30,7 +33,16 @@ defmodule AdventOfCode.Y2021.Day16 do
   @doc """
   """
   def solve_1(data) do
-    {1, :not_implemented}
+    data
+    |> decode()
+    |> sum_versions(0)
+  end
+
+  def sum_versions(%{version: version, type_id: 4}, acc), do: acc + version
+
+  def sum_versions(%{version: version, value: sub_packets}, acc) do
+    sub_packets
+    |> Enum.reduce(acc + version, fn packet, acc -> sum_versions(packet, acc) end)
   end
 
   @doc """
@@ -39,5 +51,103 @@ defmodule AdventOfCode.Y2021.Day16 do
     {2, :not_implemented}
   end
 
+  def decode(bin) when is_bitstring(bin) do
+    decode(bin, struct(__MODULE__))
+  end
+
+  def decode(<<version::3, 4::3, rest::bits>>, packet) do
+    decode_literal(rest, <<>>, 0, %{packet | version: version, type_id: 4})
+  end
+
+  def decode(
+        <<version::3, type_id::3, 0::1, bit_count::15, sub_packet_data::bits-size(bit_count),
+          rest::bits>>,
+        packet
+      ) do
+    %{
+      packet
+      | version: version,
+        type_id: type_id,
+        length_type_id: 0,
+        length: bit_count,
+        value: decode_packets(sub_packet_data, []),
+        leftover: rest
+    }
+  end
+
+  def decode(
+        <<version::3, type_id::3, 1::1, packet_count::11, rest::bits>>,
+        packet
+      ) do
+    %{
+      packet
+      | version: version,
+        type_id: type_id,
+        length_type_id: 1,
+        length: packet_count,
+        value: decode_n_packets(rest, packet_count, [])
+    }
+  end
+
+  def decode(rest, packet), do: %{packet | leftover: rest}
+
+  def decode_packets(data, packets) do
+    case decode(data) do
+      %{leftover: <<>>} = packet ->
+        Enum.reverse([packet | packets])
+
+      %{leftover: rest} = packet ->
+        decode_packets(rest, [packet | packets])
+    end
+  end
+
+  def decode_n_packets(_data, 0, packets),
+    do: Enum.reverse(packets)
+
+  def decode_n_packets(data, packet_count, packets) do
+    %{leftover: rest} = packet = decode(data)
+    decode_n_packets(rest, packet_count - 1, [packet | packets])
+  end
+
+  def decode_literal(
+        <<0::1, last_word::bits-size(4), rest::bits>>,
+        literal_bits,
+        bit_count,
+        packet
+      ) do
+    %{
+      packet
+      | leftover: rest,
+        value:
+          literal_value(
+            <<literal_bits::bits-size(bit_count), last_word::bits-size(4)>>,
+            bit_count + 4
+          )
+    }
+  end
+
+  def decode_literal(
+        <<1::1, word::bits-size(4), rest::bits>>,
+        literal_bits,
+        bit_count,
+        packet
+      ) do
+    decode_literal(
+      rest,
+      <<literal_bits::bits-size(bit_count), word::bits-size(4)>>,
+      bit_count + 4,
+      packet
+    )
+  end
+
+  defp literal_value(literal_bits, bit_count) do
+    <<value::size(bit_count)>> = literal_bits
+    value
+  end
+
+  def encode2(bits) when is_bitstring(bits),
+    do: for(<<x::size(1) <- bits>>, into: "", do: "#{x}")
+
+  def p2(bits, opts \\ []), do: tap(bits, &IO.inspect(encode2(&1), opts))
   # --- </Solution Functions> ---
 end
--- test/2021/day_16_test.exs
+++ test/2021/day_16_test.exs
@@ -9,9 +9,66 @@ defmodule AdventOfCode.Y2021.Day16Test do
   @sample_data ~S"""
   """
 
+  describe "decoding" do
+    test "b2tob16" do
+      assert "38006F45291200" ==
+               b2tob16("00111000000000000110111101000101001010010001001000000000")
+
+      assert "EE00D40C823060" ==
+               b2tob16("11101110000000001101010000001100100000100011000001100000")
+    end
+
+    test "type length id 0" do
+      # 00111000000000000110111101000101001010010001001000000000
+      packet = "38006F45291200" |> Solution.parse()
+
+      assert %Solution{version: 1, type_id: 6, length_type_id: 0, value: value} =
+               Solution.decode(packet)
+
+      assert [%Solution{type_id: 4, value: 10}, %Solution{type_id: 4, value: 20}] = value
+    end
+
+    test "type length id 1" do
+      # 11101110000000001101010000001100100000100011000001100000
+
+      packet = "EE00D40C823060" |> Solution.parse()
+
+      assert %Solution{version: 7, type_id: 3, length_type_id: 1, value: value} =
+               Solution.decode(packet)
+
+      assert [
+               %Solution{type_id: 4, value: 1},
+               %Solution{type_id: 4, value: 2},
+               %Solution{type_id: 4, value: 3}
+             ] = value
+    end
+
+    test "literals" do
+      # 110100101111111000101000
+      packet = "D2FE28" |> Solution.parse()
+
+      assert %Solution{version: 6, type_id: 4, value: 2021, leftover: <<0::3>>} =
+               Solution.decode(packet)
+
+      packet = "11010001010" |> decode2()
+      assert %Solution{type_id: 4, value: 10} = Solution.decode(packet)
+
+      packet = "0101001000100100" |> decode2()
+
+      assert %Solution{type_id: 4, value: 20} = Solution.decode(packet)
+    end
+  end
+
   describe "part 1" do
-    test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+    test "examples" do
+      for {b16, version_sum} <- [
+            {"8A004A801A8002F478", 16},
+            {"620080001611562C8802118E34", 12},
+            {"C0015000016115A2E0802F182340", 23},
+            {"A0016C880162017C3686B18A3D4780", 31}
+          ] do
+        assert version_sum == Solution.run(b16, 1)
+      end
     end
 
     test "input file" do
@@ -20,12 +77,22 @@ defmodule AdventOfCode.Y2021.Day16Test do
   end
 
   describe "part 2" do
-    test "example" do
-      assert Solution.run(@sample_data, 2) == nil
-    end
-
     test "input file" do
       assert Solution.run(2) == nil
     end
+  end
+
+  def b2tob16(bits) do
+    bits
+    |> decode2()
+    |> Base.encode16()
+  end
+
+  def decode2(bits) do
+    bits
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&<<&1::1>>)
+    |> Enum.into(<<>>)
   end
 end
