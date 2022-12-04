2dfcc1154b83e8059c6b68308122f1cea597a2c8
Lee Marlow <lee.marlow@gmail.com>
2022-12-03 19:45:29 -0700

    2022-12-03 elixir modules and tests

--- elixir/test/2022/day_03_test.exs
+++ elixir/test/2022/day_03_test.exs
@@ -7,25 +7,31 @@ defmodule AdventOfCode.Y2022.Day03Test do
   alias AdventOfCode.Y2022.Day03, as: Solution
 
   @sample_data ~S"""
+  vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 157
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 7716
     end
   end
 
   describe "part 2" do
     test "example" do
-      assert Solution.run(@sample_data, 2) == nil
+      assert Solution.run(@sample_data, 2) == 70
     end
 
     test "input file" do
-      assert Solution.run(2) == nil
+      assert Solution.run(2) == 2973
     end
   end
 end
