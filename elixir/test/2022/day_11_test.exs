02278317db16b7dbf4a13b5a6a6b10c94971c9c8
Lee Marlow <lee.marlow@gmail.com>
2022-12-14 23:06:37 -0700

    2022-12-11 sample files

---
+++ elixir/test/2022/day_11_test.exs
@@ -0,0 +1,58 @@
+defmodule AdventOfCode.Y2022.Day11Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2211
+
+  alias AdventOfCode.Y2022.Day11, as: Solution
+
+  @sample_data ~S"""
+  Monkey 0:
+    Starting items: 79, 98
+    Operation: new = old * 19
+    Test: divisible by 23
+      If true: throw to monkey 2
+      If false: throw to monkey 3
+
+  Monkey 1:
+    Starting items: 54, 65, 75, 74
+    Operation: new = old + 6
+    Test: divisible by 19
+      If true: throw to monkey 2
+      If false: throw to monkey 0
+
+  Monkey 2:
+    Starting items: 79, 60, 97
+    Operation: new = old * old
+    Test: divisible by 13
+      If true: throw to monkey 1
+      If false: throw to monkey 3
+
+  Monkey 3:
+    Starting items: 74
+    Operation: new = old + 3
+    Test: divisible by 17
+      If true: throw to monkey 0
+      If false: throw to monkey 1
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Solution.run(@sample_data, 1) == 10605
+    end
+
+    test "input file" do
+      assert Solution.run(1) == nil
+    end
+  end
+
+  describe "part 2" do
+    test "example" do
+      assert Solution.run(@sample_data, 2) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(2) == nil
+    end
+  end
+end
