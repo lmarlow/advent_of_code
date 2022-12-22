7df181fc8ef7d02aa55015479e602bfcf6d43d47
Lee Marlow <lee.marlow@gmail.com>
2022-12-21 21:58:38 -0700

    2022-12-14 problem files

---
+++ elixir/test/2022/day_14_test.exs
@@ -0,0 +1,33 @@
+defmodule AdventOfCode.Y2022.Day14Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2214
+
+  alias AdventOfCode.Y2022.Day14, as: Solution
+
+  @sample_data ~S"""
+  498,4 -> 498,6 -> 496,6
+  503,4 -> 502,4 -> 502,9 -> 494,9
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Solution.run(@sample_data, 1) == 24
+    end
+
+    test "input file" do
+      assert Solution.run(1) == nil
+    end
+  end
+
+  describe "part 2" do
+    test "example" do
+      assert Solution.run(@sample_data, 2) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(2) == nil
+    end
+  end
+end
