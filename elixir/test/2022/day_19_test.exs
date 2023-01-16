f275edfe7b87303ecdd6639e94530cce040d506d
Lee Marlow <lee.marlow@gmail.com>
2023-01-16 12:44:37 -0700

    2022-12-19 add files

---
+++ elixir/test/2022/day_19_test.exs
@@ -0,0 +1,31 @@
+defmodule AdventOfCode.Y2022.Day19Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2219
+
+  alias AdventOfCode.Y2022.Day19, as: Solution
+
+  @sample_data ~S"""
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Solution.run(@sample_data, 1) == 33
+    end
+
+    test "input file" do
+      assert Solution.run(1) == nil
+    end
+  end
+
+  describe "part 2" do
+    test "example" do
+      assert Solution.run(@sample_data, 2) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(2) == nil
+    end
+  end
+end
