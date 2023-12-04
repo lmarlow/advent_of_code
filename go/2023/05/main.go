58112e947ad86c295826ead94e5daaf9f1ed9353
Lee Marlow <lee.marlow@gmail.com>
2023-12-04 12:36:17 -0700

    boilerplate for 2023-05

---
+++ go/2023/05/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 2 {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	ans := 0
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	return ans
+}
+
+func part2(input string) int {
+	ans := 0
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	return ans
+}
