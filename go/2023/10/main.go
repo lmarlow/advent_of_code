a2a685ddceb99d692f8738245d7767645816531c
Lee Marlow <lee.marlow@gmail.com>
2023-12-14 00:50:44 -0700

    2023-10 part 2

--- go/2023/10/main.go
+++ go/2023/10/main.go
@@ -35,10 +35,10 @@ func main() {
 }
 
 var (
-	north = []byte{'|', '7', 'F'}
-	east  = []byte{'-', 'J', '7'}
-	south = []byte{'|', 'L', 'J'}
-	west  = []byte{'-', 'L', 'F'}
+	north = []rune{'|', '7', 'F'}
+	east  = []rune{'-', 'J', '7'}
+	south = []rune{'|', 'L', 'J'}
+	west  = []rune{'-', 'L', 'F'}
 )
 
 type IntPair struct {
@@ -62,7 +62,7 @@ func (p IntPair) West() IntPair {
 	return IntPair{p.X - 1, p.Y}
 }
 
-func (p IntPair) Neighbors(pipe byte) (ns []IntPair) {
+func (p IntPair) Neighbors(pipe rune) (ns []IntPair) {
 	switch pipe {
 	case 'S':
 		ns = []IntPair{p.North(), p.East(), p.South(), p.West()}
@@ -84,7 +84,7 @@ func (p IntPair) Neighbors(pipe byte) (ns []IntPair) {
 	return
 }
 
-func (p IntPair) Next(prevXY IntPair, currentPipe byte) IntPair {
+func (p IntPair) Next(prevXY IntPair, currentPipe rune) IntPair {
 	if ns := p.Neighbors(currentPipe); ns[0] == prevXY {
 		return ns[1]
 	} else {
@@ -93,13 +93,13 @@ func (p IntPair) Next(prevXY IntPair, currentPipe byte) IntPair {
 }
 
 func part1(input string) (ans int) {
-	pipes := make(map[IntPair]byte)
+	pipes := make(map[IntPair]rune)
 	var startXY, prevXY, headXY IntPair
 	for y, line := range strings.Split(input, "\n") {
 		for x, b := range line {
 			if b != '.' {
 				xy := IntPair{x, y}
-				pipes[xy] = byte(b)
+				pipes[xy] = b
 				if b == 'S' {
 					startXY = xy
 					prevXY = startXY
@@ -108,7 +108,7 @@ func part1(input string) (ans int) {
 		}
 	}
 
-	var currentPipe byte
+	var currentPipe rune
 	var ok bool
 	if currentPipe, ok = pipes[startXY.North()]; ok && slices.Contains(north, currentPipe) {
 		headXY = startXY.North()
@@ -132,8 +132,109 @@ func part1(input string) (ans int) {
 	return
 }
 
+func determineS(path []IntPair) (s rune) {
+	last, start, first := path[len(path)-1], path[0], path[1]
+	// fmt.Println(last, first)
+	if (last.North() == start && start.North() == first) || (last.South() == start && start.South() == first) {
+		s = '|'
+	} else if (last.East() == start && start.East() == first) || (last.West() == start && start.West() == first) {
+		s = '-'
+	} else if (last.East() == start && start.North() == first) || (last.South() == start && start.West() == first) {
+		s = 'J'
+	} else if (last.East() == start && start.South() == first) || (last.North() == start && start.West() == first) {
+		s = '7'
+	} else if (last.West() == start && start.South() == first) || (last.North() == start && start.East() == first) {
+		s = 'F'
+	} else if (last.South() == start && start.East() == first) || (last.West() == start && start.North() == first) {
+		s = 'L'
+	}
+	return
+}
+
 func part2(input string) (ans int) {
+	pipes := make(map[IntPair]rune)
+	var startXY, prevXY, headXY IntPair
 	lines := strings.Split(input, "\n")
-	ans = len(lines)
+	for y, line := range lines {
+		for x, b := range line {
+			if b != '.' {
+				xy := IntPair{x, y}
+				pipes[xy] = b
+				if b == 'S' {
+					startXY = xy
+					prevXY = startXY
+				}
+			}
+		}
+	}
+
+	var currentPipe rune
+	var ok bool
+	if currentPipe, ok = pipes[startXY.North()]; ok && slices.Contains(north, currentPipe) {
+		headXY = startXY.North()
+	} else if currentPipe, ok = pipes[startXY.East()]; ok && slices.Contains(east, currentPipe) {
+		headXY = startXY.East()
+	} else if currentPipe, ok = pipes[startXY.South()]; ok && slices.Contains(south, currentPipe) {
+		headXY = startXY.South()
+	} else if currentPipe, ok = pipes[startXY.West()]; ok && slices.Contains(west, currentPipe) {
+		headXY = startXY.West()
+	}
+	currentPipe = pipes[headXY]
+
+	loopIndex := 0
+	xy2Index := map[IntPair]int{startXY: loopIndex}
+	path := []IntPair{startXY}
+	for headXY != startXY {
+		prevXY, headXY = headXY, headXY.Next(prevXY, currentPipe)
+		currentPipe = pipes[headXY]
+		loopIndex++
+		xy2Index[prevXY] = loopIndex
+		path = append(path, prevXY)
+	}
+
+	dimX, dimY := len(lines[0]), len(lines)
+	sPipe := determineS(path)
+	// fmt.Printf("S = %c\n", sPipe)
+
+	for y := 0; y < dimY; y++ {
+		crossings := 0
+		beginCrossing := '.'
+		for x := 0; x < dimX; x++ {
+			xy := IntPair{x, y}
+			if _, ok := xy2Index[xy]; ok {
+				r := rune(lines[y][x])
+				if xy == startXY {
+					r = sPipe
+				}
+				switch r {
+				case 'F', 'L':
+					beginCrossing = r
+				case '|':
+					crossings++
+					beginCrossing = '.'
+				case 'J':
+					if beginCrossing == 'F' {
+						crossings++
+						beginCrossing = '.'
+					}
+				case '7':
+					if beginCrossing == 'L' {
+						crossings++
+						beginCrossing = '.'
+					}
+				}
+				// fmt.Print(string(pipes[xy]))
+			} else {
+				if crossings%2 == 1 {
+					// fmt.Print("I")
+					ans++
+				} else {
+					// fmt.Print("O")
+				}
+			}
+		}
+		// fmt.Println()
+	}
+
 	return
 }
--- go/2023/10/main_test.go
+++ go/2023/10/main_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 )
 
+// ..F7.
+// .FJ|.
+// SJ.L7
+// |F--J
+// LJ...
 var sample = `..F7.
 .FJ|.
 SJ.L7
@@ -36,22 +41,69 @@ func Test_part1(t *testing.T) {
 	}
 }
 
+var sampleP2S2 = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`
+
+var sampleP2S3 = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
+
+var sampleP2S4 = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`
+
 func Test_part2(t *testing.T) {
 	tests := []struct {
 		name  string
 		input string
 		want  int
 	}{
-		// {
-		// 	name:  "sample",
-		// 	input: sample,
-		// 	want:  -1,
-		// },
-		// {
-		// 	name:  "actual",
-		// 	input: input,
-		// 	want:  -1,
-		// },
+		{
+			name:  "sample",
+			input: sample,
+			want:  1,
+		},
+		{
+			name:  "sample",
+			input: sampleP2S2,
+			want:  4,
+		},
+		{
+			name:  "sample",
+			input: sampleP2S3,
+			want:  8,
+		},
+		{
+			name:  "sample",
+			input: sampleP2S4,
+			want:  10,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  325,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
