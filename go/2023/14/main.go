70c177e35fc797675897e5363fe1e81c5d839a99
Lee Marlow <lee.marlow@gmail.com>
2023-12-27 12:07:32 -0700

    refactor to get ready for 2023-14 part 2

--- go/2023/14/main.go
+++ go/2023/14/main.go
@@ -33,22 +33,62 @@ func main() {
 	}
 }
 
-func part1(input string) (ans int) {
-	lines := strings.Split(input, "\n")
-	n := len(lines)
+const (
+	DOT = iota
+	ROLLER
+	STATIC
+)
 
-	obstructions := make([]int, len(lines[0]))
-
-	for j, row := range lines {
+func north(platform [][]int) {
+	obstructions := make([]int, len(platform))
+	for j, row := range platform {
 		// fmt.Println(obstructions)
 		for i := range row {
 			switch row[i] {
-			case '#':
+			case STATIC:
 				obstructions[i] = j + 1
-			case 'O':
-				ans += n - obstructions[i]
+			case ROLLER:
+				platform[j][i] = DOT
+				platform[obstructions[i]][i] = ROLLER
 				// fmt.Println(j, i, obstructions[i])
 				obstructions[i]++
+			}
+		}
+	}
+}
+
+func parsePlatform(input string) [][]int {
+	lines := strings.Split(input, "\n")
+	nRows := len(lines)
+	nCols := len(lines[0])
+	platform := make([][]int, nRows)
+	for j, row := range lines {
+		platform[j] = make([]int, nCols)
+		for i := range row {
+			switch row[i] {
+			case '#':
+				platform[j][i] = STATIC
+			case 'O':
+				platform[j][i] = ROLLER
+			default:
+				platform[j][i] = DOT
+			}
+		}
+	}
+
+	return platform
+}
+
+func part1(input string) (ans int) {
+	platform := parsePlatform(input)
+	nRows := len(platform)
+
+	north(platform)
+
+	for j, row := range platform {
+		for i := range row {
+			if row[i] == ROLLER {
+				ans += nRows - j
 			}
 		}
 	}
