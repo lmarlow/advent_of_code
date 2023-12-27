62a444f1262e72052d5bb17160061b060b7ec672
Lee Marlow <lee.marlow@gmail.com>
2023-12-26 17:37:32 -0700

    2023-13 part 1

--- go/2023/13/main.go
+++ go/2023/13/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"log"
 	"strings"
 )
 
@@ -33,10 +34,65 @@ func main() {
 	}
 }
 
-func part1(input string) (ans int) {
-	lines := strings.Split(input, "\n")
+func rowsCols(field string) (rows []string, cols []string) {
+	rows = strings.Split(field, "\n")
 
-	ans = len(lines)
+	for i := 0; i < len(rows[0]); i++ {
+		col := make([]byte, len(rows))
+		for j := 0; j < len(rows); j++ {
+			col = append(col, rows[j][i])
+		}
+		cols = append(cols, string(col))
+	}
+
+	return
+}
+
+func reflectsAround(rows []string) (idx int) {
+	n := len(rows)
+	for i := 1; i < n; i++ {
+		isReflected := true
+		x := min(i, n-i)
+		for j := 0; isReflected && j < x; j++ {
+			// fmt.Println(i, j, n, x, i-(j+1), rows[i-(j+1)], i+j, rows[i+j])
+			isReflected = isReflected && (rows[i-(j+1)] == rows[i+j])
+		}
+		if isReflected {
+			return i
+		}
+	}
+	return
+}
+
+func fieldValue(rows []string, cols []string) (ans int) {
+	// fmt.Println("checking vertical")
+	if ans = reflectsAround(cols); ans > 0 {
+		// fmt.Println("cols", ans)
+		return
+	}
+
+	// fmt.Println("checking horizontal")
+	ans = reflectsAround(rows)
+	if ans > 0 {
+		// fmt.Println("rows", ans)
+	}
+	ans *= 100
+	return
+}
+
+func part1(input string) (ans int) {
+	fields := strings.Split(input, "\n\n")
+
+	for i, f := range fields {
+		// fmt.Println(f)
+		rows, cols := rowsCols(f)
+		fv := fieldValue(rows, cols)
+		if fv == 0 {
+			log.Fatalf("\n%d: No reflection found for %dx%d field\n%s\n", i, len(cols), len(rows), f)
+		}
+		ans += fv
+	}
+
 	return
 }
 
