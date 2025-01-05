5846d2d8716b41f63654b88a1b9be59a01293600
Lee Marlow <lee.marlow@gmail.com>
2025-01-05 09:23:12 -0700

    2024-02 part 1

--- elixir/test/2024/day_02_test.exs
+++ elixir/test/2024/day_02_test.exs
@@ -7,15 +7,21 @@ defmodule AdventOfCode.Y2024.Day02Test do
   alias AdventOfCode.Y2024.Day02, as: Solution
 
   @sample_data ~S"""
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 2
     end
 
     test "input file" do
-      assert Solution.run(1) == nil
+      assert Solution.run(1) == 591
     end
   end
 
