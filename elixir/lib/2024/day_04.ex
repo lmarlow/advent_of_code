67a928ea3b2cbfb654c82d31e268a56b4c048f57
Lee Marlow <lee.marlow@gmail.com>
2025-01-06 00:40:41 -0700

    mix solve --year 2024 --day 4

---
+++ elixir/lib/2024/day_04.ex
@@ -0,0 +1,99 @@
+defmodule AdventOfCode.Y2024.Day04 do
+  @moduledoc """
+  # Day 4: Ceres Search
+
+  Problem Link: https://adventofcode.com/2024/day/4
+  """
+  use AdventOfCode.Helpers.InputReader, year: 2024, day: 4
+
+  @doc ~S"""
+  Sample data:
+
+  ```
+  ```
+  """
+  def run(data \\ input!(), part)
+
+  def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
+
+  def run(data, part) when is_list(data), do: data |> solve(part)
+
+  def parse(data) do
+    data
+    |> String.split("\n", trim: true)
+  end
+
+  def solve(data, 1), do: solve_1(data)
+  def solve(data, 2), do: solve_2(data)
+
+  # --- <Solution Functions> ---
+
+  @doc """
+  # Part 1
+
+  "Looks like the Chief's not here. Next!" One of The Historians pulls out
+  a device and pushes the only button on it. After a brief flash, you
+  recognize the interior of the [Ceres monitoring station](/2019/day/10)!
+
+  As the search for the Chief continues, a small Elf who lives on the
+  station tugs on your shirt; she'd like to know if you could help her
+  with her *word search* (your puzzle input). She only has to find one
+  word: `XMAS`.
+
+  This word search allows words to be horizontal, vertical, diagonal,
+  written backwards, or even overlapping other words. It's a little
+  unusual, though, as you don't merely need to find one instance of
+  `XMAS` - you need to find *all of them*. Here are a few ways `XMAS`
+  might appear, where irrelevant characters have been replaced with `.`:
+
+    ..X...
+    .SAMX.
+    .A..A.
+    XMAS.S
+    .X....
+
+  The actual word search will be full of letters instead. For example:
+
+    MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX
+
+  In this word search, `XMAS` occurs a total of *`18`* times; here's the
+  same word search again, but where letters not involved in any `XMAS`
+  have been replaced with `.`:
+
+    ....XXMAS.
+    .SAMXMS...
+    ...S..A...
+    ..A.A.MS.X
+    XMASAMX.MM
+    X.....XA.A
+    S.S.S.S.SS
+    .A.A.A.A.A
+    ..M.M.M.MM
+    .X.X.XMASX
+
+  Take a look at the little Elf's word search. *How many times does `XMAS`
+  appear?*
+
+  """
+  def solve_1(_data) do
+    {1, :not_implemented}
+  end
+
+  @doc """
+  # Part 2
+  """
+  def solve_2(_data) do
+    {2, :not_implemented}
+  end
+
+  # --- </Solution Functions> ---
+end
---
+++ elixir/test/2024/day_04_test.exs
@@ -0,0 +1,31 @@
+defmodule AdventOfCode.Y2024.Day04Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2404
+
+  alias AdventOfCode.Y2024.Day04, as: Solution
+
+  @sample_data ~S"""
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Solution.run(@sample_data, 1) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(1) == nil
+    end
+  end
+
+  describe "part 2" do
+    test "example" do
+      assert Solution.run(@sample_data, 2) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(2) == nil
+    end
+  end
+end
