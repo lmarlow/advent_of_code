7ebc802afe9a6b1d2f25b529d44b6a1ead0d45cb
Lee Marlow <lee.marlow@gmail.com>
2025-01-06 00:42:25 -0700

    2024-04 add sample data test for part 1

--- elixir/test/2024/day_04_test.exs
+++ elixir/test/2024/day_04_test.exs
@@ -7,11 +7,21 @@ defmodule AdventOfCode.Y2024.Day04Test do
   alias AdventOfCode.Y2024.Day04, as: Solution
 
   @sample_data ~S"""
+  MMMSXXMASM
+  MSAMXMSMSA
+  AMXSXMAAMM
+  MSAMASMSMX
+  XMASAMXAMM
+  XXAMMXXAMA
+  SMSMSASXSS
+  SAXAMASAAA
+  MAMMMXMMMM
+  MXMXAXMASX
   """
 
   describe "part 1" do
     test "example" do
-      assert Solution.run(@sample_data, 1) == nil
+      assert Solution.run(@sample_data, 1) == 18
     end
 
     test "input file" do
