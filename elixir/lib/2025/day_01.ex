d707492cc6a02d3ab7862f719f4cb841e817ad2d
Lee Marlow <lee.marlow@gmail.com>
2025-12-04 08:38:48 -0700

    2025-01 part 1

---
+++ elixir/lib/2025/day_01.ex
@@ -0,0 +1,161 @@
+defmodule AdventOfCode.Y2025.Day01 do
+  @moduledoc """
+  # Day 1: Secret Entrance
+
+  Problem Link: https://adventofcode.com/2025/day/1
+  """
+  use AdventOfCode.Helpers.InputReader, year: 2025, day: 1
+
+  @doc ~S"""
+  Sample data:
+
+  ```
+  ```
+  """
+  def run(data \\ input!(), part)
+
+  def run(data, part) when is_binary(data), do: data |> parse() |> run(part)
+
+  def run(data, part) when is_list(data), do: data |> solve(part)
+
+  def parse(data) do
+    data
+    |> String.split("\n", trim: true)
+  end
+
+  def solve(data, 1), do: solve_1(data)
+  def solve(data, 2), do: solve_2(data)
+
+  # --- <Solution Functions> ---
+
+  @doc """
+  # Part 1
+
+  The Elves have good news and bad news.
+
+  The good news is that they've discovered
+  <a href="https://en.wikipedia.org/wiki/Project_management"
+  target="_blank">project management</a>! This has given them the tools
+  they need to prevent their usual Christmas emergency. For example, they
+  now know that the North Pole decorations need to be finished soon so
+  that other critical tasks can start on time.
+
+  The bad news is that they've realized they have a *different* emergency:
+  according to their resource planning, none of them have any time left to
+  decorate the North Pole!
+
+  To save Christmas, the Elves need *you* to *finish decorating the North
+  Pole by December 12th*.
+
+  Collect stars by solving puzzles. Two puzzles will be made available on
+  each day; the second puzzle is unlocked when you complete the first.
+  Each puzzle grants *one star*. Good luck!
+
+  You arrive at the secret entrance to the North Pole base ready to start
+  decorating. Unfortunately, the *password* seems to have been changed, so
+  you can't get in. A document taped to the wall helpfully explains:
+
+  "Due to new security protocols, the password is locked in the safe
+  below. Please see the attached document for the new combination."
+
+  The safe has a dial with only an arrow on it; around the dial are the
+  numbers `0` through `99` in order. As you turn the dial, it makes a
+  small *click* noise as it reaches each number.
+
+  The attached document (your puzzle input) contains a sequence of
+  *rotations*, one per line, which tell you how to open the safe. A
+  rotation starts with an `L` or `R` which indicates whether the rotation
+  should be to the *left* (toward lower numbers) or to the *right* (toward
+  higher numbers). Then, the rotation has a *distance* value which
+  indicates how many clicks the dial should be rotated in that direction.
+
+  So, if the dial were pointing at `11`, a rotation of `R8` would cause
+  the dial to point at `19`. After that, a rotation of `L19` would cause
+  it to point at `0`.
+
+  Because the dial is a circle, turning the dial *left from `0`* one click
+  makes it point at `99`. Similarly, turning the dial *right from `99`*
+  one click makes it point at `0`.
+
+  So, if the dial were pointing at `5`, a rotation of `L10` would cause it
+  to point at `95`. After that, a rotation of `R5` could cause it to point
+  at `0`.
+
+  The dial starts by pointing at `50`.
+
+  You could follow the instructions, but your recent required official
+  North Pole secret entrance security training seminar taught you that the
+  safe is actually a decoy. The actual password is *the number of times
+  the dial is left pointing at `0` after any rotation in the sequence*.
+
+  For example, suppose the attached document contained the following
+  rotations:
+
+      L68
+      L30
+      R48
+      L5
+      R60
+      L55
+      L1
+      L99
+      R14
+      L82
+
+  Following these rotations would cause the dial to move as follows:
+
+  - The dial starts by pointing at `50`.
+  - The dial is rotated `L68` to point at `82`.
+  - The dial is rotated `L30` to point at `52`.
+  - The dial is rotated `R48` to point at *`0`*.
+  - The dial is rotated `L5` to point at `95`.
+  - The dial is rotated `R60` to point at `55`.
+  - The dial is rotated `L55` to point at *`0`*.
+  - The dial is rotated `L1` to point at `99`.
+  - The dial is rotated `L99` to point at *`0`*.
+  - The dial is rotated `R14` to point at `14`.
+  - The dial is rotated `L82` to point at `32`.
+
+  Because the dial points at `0` a total of three times during this
+  process, the password in this example is *`3`*.
+
+  Analyze the rotations in your attached document. *What's the actual
+  password to open the door?*
+
+  """
+  def solve_1(data) do
+    {_dial, total} =
+      for <<dir::binary-size(1), count::binary>> <- data,
+          count = String.to_integer(count),
+          reduce: {50, 0} do
+        {dial, total} ->
+          dial =
+            if dir == "R" do
+              dial + count
+            else
+              dial - count
+            end
+            |> Integer.mod(100)
+
+          total =
+            if dial == 0 do
+              total + 1
+            else
+              total
+            end
+
+          {dial, total}
+      end
+
+    total
+  end
+
+  @doc """
+  # Part 2
+  """
+  def solve_2(_data) do
+    {2, :not_implemented}
+  end
+
+  # --- </Solution Functions> ---
+end
---
+++ elixir/test/2025/day_01_test.exs
@@ -0,0 +1,41 @@
+defmodule AdventOfCode.Y2025.Day01Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2501
+
+  alias AdventOfCode.Y2025.Day01, as: Solution
+
+  @sample_data ~S"""
+  L68
+  L30
+  R48
+  L5
+  R60
+  L55
+  L1
+  L99
+  R14
+  L82
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Solution.run(@sample_data, 1) == 3
+    end
+
+    test "input file" do
+      assert Solution.run(1) == 1141
+    end
+  end
+
+  describe "part 2" do
+    test "example" do
+      assert Solution.run(@sample_data, 2) == nil
+    end
+
+    test "input file" do
+      assert Solution.run(2) == nil
+    end
+  end
+end
